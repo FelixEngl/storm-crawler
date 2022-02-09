a9ba525cefd9a9614b469e9e58c4cbd62530d043
Felix Engl <felix.engl@hotmail.com>
2022-02-09 16:41:07 +0100

    Return more pollution
    Signed-off-by: Felix Engl <felix.engl@hotmail.com>

--- core/src/main/java/com/digitalpebble/stormcrawler/Metadata.java
+++ core/src/main/java/com/digitalpebble/stormcrawler/Metadata.java
@@ -101,7 +101,6 @@ public class Metadata {
 
     /** Returns the first value for the key or null if it does not exist, given a prefix */
     @Contract(pure = true)
-    @Nullable
     public String getFirstValue(@NotNull String key, @Nullable String prefix) {
         if (isNullOrEmpty(prefix)) return getFirstValue(key);
         return getFirstValue(prefix + key);
@@ -112,7 +111,6 @@ public class Metadata {
      * getValues(prefix+key)} It ignores the prefix if it is null or empty.
      */
     @Contract(pure = true)
-    @Nullable
     public String[] getValues(@NotNull String key, @Nullable String prefix) {
         if (isNullOrEmpty(prefix)) return getValues(key);
         return getValues(prefix + key);
@@ -120,7 +118,6 @@ public class Metadata {
 
     /** Returns the array of values for the given {@code key}. */
     @Contract(pure = true)
-    @Nullable
     public String[] getValues(@NotNull String key) {
         String[] values = map.get(key);
         if (values == null || values.length == 0) return null;
@@ -150,7 +147,6 @@ public class Metadata {
 
     /** Internal set method without safety check. */
     @Contract(mutates = "this")
-    @Nullable
     private String[] putValueInternal(@NotNull String key, @Nullable String value) {
         return map.put(key, new String[] {value});
     }
@@ -226,7 +222,6 @@ public class Metadata {
      *     implementation supports null values.)
      */
     @Contract(mutates = "this")
-    @Nullable
     public String[] setValue(@NotNull String key, @Nullable String value) {
         checkLockException();
         return putValueInternal(key, value);
